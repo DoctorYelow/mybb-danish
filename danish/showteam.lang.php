dc3fc667e7636ddb0208321aefe7d0744e27b66d
themagtime <magtime123456@gmail.com>
2015-07-17 23:34:53 +0200

    Translated showteam.lang.php

---
+++ danish/showteam.lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_showteam'] = "Forum Team";
+$l['forum_team'] = "Forum Team";
+$l['moderators'] = "Moderatorer";
+$l['username'] = "Brugernavn";
+$l['lastvisit'] = "Sidste login";
+$l['email'] = "Email";
+$l['pm'] = "Privat besked";
+$l['mod_forums'] = "Forum(er)";
+$l['online'] = "Online";
+$l['offline'] = "Offline";
+
+$l['group_leaders'] = "Leder(e)";
+$l['group_members'] = "Medlem(er)";
+
+$l['no_members'] = "Ingen medlemmer i denne gruppe";
+
+$l['error_noteamstoshow'] = "Der er intet forum team at vise.";
